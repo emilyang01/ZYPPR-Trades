138f529e9a126ad53c7ccf25ca1479b7e8ae8085
Emily Yang <136683472+emilyang01@users.noreply.github.com>
2025-11-19 11:07:13 -0800

    Add user model and user routes

--- mern-stack/backend/server.js
+++ mern-stack/backend/server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-
-
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 console.log("MONGO_URI:", process.env.MONGO_URI); // Debug
@@ -10,9 +9,13 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
+
+app.use("api/users", userRoutes);
 
 app.listen(5000, () => {
   console.log("Server started at http://localhost:5000");
