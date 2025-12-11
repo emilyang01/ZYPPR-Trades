5cd4bc55772564f7416755f12dc6d6b404a21a31
Phillips <melodyphillips2@csus.edu>
2025-12-11 13:18:40 -0800

    notification schema update

--- mern-stack/backend/models/notifications.js
+++ mern-stack/backend/models/notifications.js
@@ -21,47 +21,4 @@ const NotificationSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-
-  // TYPE OF NOTIFICATION
-  type: {
-    type: String,
-    enum: [
-      "message",
-      "job_application",
-      "job_accepted",
-      "job_rejected",
-      "review_received",
-      "follower",
-      "system",
-    ],
-    required: true
-  },
-
-  // OPTIONAL REFERENCES (depending on notification type)
-  job: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "job"
-  },
-
-  messageRef: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "messages"
-  },
-
-  review: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "review"
-  },
-
-  // READ STATUS
-  read: {
-    type: Boolean,
-    default: false
-  },
-
-  // OPTIONAL extra data
-  metadata: {
-    type: mongoose.Schema.Types.Mixed
-  }
-
 }, { timestamps: true });
